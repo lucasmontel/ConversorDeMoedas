6c30683ebb76331f6576b8e2c70927d3d3095cad
lucasmontel <aimpostor6@gmail.com>
2023-09-24 15:24:28 -0300

    #

--- bootstrap/js/script.js
+++ bootstrap/js/script.js
@@ -24,12 +24,35 @@ fetch(url + coins) // Fazemos a  requisição, após isso, a transformamos em fo
 
       if (optionOne === "dolar" && optionTwo === "real") {
         inputResultado.value = dolarReal * quantidadeOne.value;
+      } else if (optionOne === "real" && optionTwo === "dolar") {
+        inputResultado.value = quantidadeOne.value / dolarReal;
       } else if (optionOne === "euro" && optionTwo === "real") {
         inputResultado.value = euroReal * quantidadeOne.value;
+      } else if (optionOne === "real" && optionTwo === "euro") {
+        inputResultado.value = quantidadeOne.value / euroReal;
       } else if (optionOne === "bitcoin" && optionTwo === "real") {
         inputResultado.value = btcReal * quantidadeOne.value;
+      } else if (optionOne === "real" && optionTwo === "bitcoin") {
+        inputResultado.value = quantidadeOne.value / btcReal;
       }
-       }
+
+      // Combinações sem "real"
+      if (optionOne === "dolar" && optionTwo === "euro") {
+        inputResultado.value = (dolarReal * quantidadeOne.value) / euroReal;
+      } else if (optionOne === "euro" && optionTwo === "dolar") {
+        inputResultado.value = (euroReal * quantidadeOne.value) / dolarReal;
+      } else if (optionOne === "dolar" && optionTwo === "bitcoin") {
+        inputResultado.value = (dolarReal * quantidadeOne.value) / btcReal;
+      } else if (optionOne === "bitcoin" && optionTwo === "dolar") {
+        inputResultado.value = (btcReal * quantidadeOne.value) / dolarReal;
+      } else if (optionOne === "euro" && optionTwo === "bitcoin") {
+        inputResultado.value = (euroReal * quantidadeOne.value) / btcReal;
+      } else if (optionOne === "bitcoin" && optionTwo === "euro") {
+        inputResultado.value = (btcReal * quantidadeOne.value) / euroReal;
+      }
+
+      // Adicione aqui mais combinações se necessário.
+    }
   });
 
 // Veja se eu entendi sobre esse código, primeiro selecionamos os nossos select, quando uma das opções dentro
